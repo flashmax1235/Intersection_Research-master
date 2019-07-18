ac61f5e274855e5e3bbc2ef4ef11dfdb277abea9
Maxwell Gordon <maxwellalexgordon@gmail.com>
2019-07-18 16:34:31 -0600

    added quadrents and right turn
    line skipping issue, must be addreseed

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Intersection_Research-master',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='maxwell',
+    author_email='',
+    description=''
+)
